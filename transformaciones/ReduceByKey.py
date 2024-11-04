72b63d6c6831d174c98552d8861c7555a08e717c
Jun1or11 <ramosflores135@gmail.com>
2024-11-03 23:16:47 -0500

    Update ReduceByKey.py

--- transformaciones/ReduceByKey.py
+++ transformaciones/ReduceByKey.py
@@ -5,7 +5,7 @@ sc = SparkContext("local", "ReduceByKey Example")
 # Datos
 datos = [("Gato", 1), ("Pez", 1), ("Gato", 1), ("Perro", 1), ("Pez", 1), ("Perro", 1), ("Perro", 1)]
 
-# Creamos RDDs
+# Creamos RDD
 rdd = sc.parallelize(datos)
 
 # Usamos reduceByKey para contabilisar
