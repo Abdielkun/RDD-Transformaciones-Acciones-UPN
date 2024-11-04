57a3b81c73202626af1c81c1ffbcde17ce9c3e91
Jun1or11 <ramosflores135@gmail.com>
2024-11-03 22:54:27 -0500

    Create ReduceByKey.py

---
+++ transformaciones/ReduceByKey.py
@@ -0,0 +1,18 @@
+from pyspark import SparkContext
+
+sc = SparkContext("local", "ReduceByKey Example")
+
+# Datos
+datos = [("Gato", 1), ("Pez", 1), ("Gato", 1), ("Perro", 1), ("Pez", 1), ("Perro", 1), ("Perro", 1)]
+
+# Creamos RDDs
+rdd = sc.parallelize(datos)
+
+# Usamos reduceByKey para contabilisar
+rdd_reducido = rdd.reduceByKey(lambda a, b: a + b)
+
+# mostramos el resultado
+resultado = rdd_reducido.collect()
+print("Conteo por categoría:", resultado)
+
+sc.stop()
